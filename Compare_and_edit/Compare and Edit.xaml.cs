721d956738e7790aeb2ec3add245190577825770
Luc Wicki <Luc_Wicki@sluz.ch>
2022-06-20 10:51:58 +0200

    Alles bis zur compare funktion

--- Compare_and_edit/Compare and Edit.xaml.cs
+++ Compare_and_edit/Compare and Edit.xaml.cs
@@ -19,6 +19,14 @@ using Button = System.Windows.Controls.Button;
 
 namespace Compare_and_edit
 {
+
+    public class RowType
+    {
+        public string Filename { get; set; }
+        public string Date { get; set; }
+        public string FileSize { get; set; }
+        public bool Identical { get; set; }
+    }
     /// <summary>
     /// Interaktionslogik für Compare_and_Edit.xaml
     /// </summary>
@@ -49,34 +57,92 @@ namespace Compare_and_edit
             Foldercontent.Columns.Add(textColumn4);
         }
 
-        public void OpenExplorer(object sender, RoutedEventArgs args)
+        //https://stackoverflow.com/questions/14488796/does-net-provide-an-easy-way-convert-bytes-to-kb-mb-gb-etc
+        static readonly string[] SizeSuffixes =
+                   { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        static string SizeSuffix(Int64 value, int decimalPlaces = 1)
         {
-            System.Windows.Forms.FolderBrowserDialog openFileDlg = new System.Windows.Forms.FolderBrowserDialog();
-            var result = openFileDlg.ShowDialog();
-            string myFolder = ((Button)sender).Tag.ToString();
-            MessageBox.Show(myFolder);
-            if (result.ToString() != string.Empty)
+            if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
+            if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
             {
-                if (myFolder == "Folder1")
-                {
-                    txtPath1.Text = openFileDlg.SelectedPath;
-                }
-                if (myFolder == "Folder2")
-                {
-                    txtPath2.Text = openFileDlg.SelectedPath;
-                }
+                mag += 1;
+                adjustedSize /= 1024;
             }
 
-            //string varPath = openFileDlg.SelectedPath;
-            //string[] fileFullNames = Directory.GetFiles(varPath);
-
-            //for (int i = 0; i < fileFullNames.Length; i++)
-            //{
-            //    Foldercontent.(fileFullNames[i].Substring(varPath.Length, fileFullNames[i].Length - varPath.Length), Directory.GetCreationTime(fileFullNames[i]).ToString());
-            //}
-            //MessageBox.Show(Foldercontent.Columns.Count.ToString());
-
-            //Process.Start("explorer.exe", @"c:\folder");
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                SizeSuffixes[mag]);
         }
+        public void Folder1Clickedtxt(object sender, RoutedEventArgs args)
+        {
+            System.Windows.Forms.FolderBrowserDialog openFileDlg = new System.Windows.Forms.FolderBrowserDialog();
+            openFileDlg.ShowDialog();
+            txtPath1.Text = openFileDlg.SelectedPath;
+            string pathcheck = txtPath1.Text;
+            updateDatagrid(openFileDlg.SelectedPath);
+
+        }
+        public void Folder2Clickedtxt(object sender, RoutedEventArgs args)
+        {
+            System.Windows.Forms.FolderBrowserDialog openFileDlg = new System.Windows.Forms.FolderBrowserDialog();
+            openFileDlg.ShowDialog();
+            txtPath2.Text = openFileDlg.SelectedPath;
+            updateDatagrid(openFileDlg.SelectedPath);
+        }
+        public void Folder1Clicked(object sender, RoutedEventArgs args) {
+            updateDatagrid(txtPath1.Text);
+
+
+        }
+        public void Folder2Clicked(object sender, RoutedEventArgs args)
+        {
+            updateDatagrid(txtPath2.Text);
+        }
+       
+
+        public void updateDatagrid(string path)
+        {
+
+            //Datagird clearen
+            Foldercontent.Items.Clear();
+            Foldercontent.Items.Refresh();
+            // get the file attributes for file or directory
+                FileAttributes attr = File.GetAttributes(path); //Fehlermeldung System.ArgumentException: "Der Pfad hat ein ungültiges Format." lösen
+
+
+            //wenn es kein Ordner ist geh zurück/ brich ab
+            if (!attr.HasFlag(FileAttributes.Directory))
+                return;
+            //Datagrid befüllen
+            string [] dirContent = Directory.GetFileSystemEntries(path);
+            foreach (string entry in dirContent)
+            {
+                FileAttributes attri = File.GetAttributes(entry);
+
+                long length = 0;
+                if (!attri.HasFlag(FileAttributes.Directory))
+                {
+                    length = new System.IO.FileInfo(entry).Length;
+                }
+                string SizeOut = SizeSuffix(length);
+                var lastModified = System.IO.File.GetLastWriteTime(entry);
+                string NameOnly = System.IO.Path.GetFileName(entry);
+
+                Foldercontent.Items.Add(new RowType() { Filename = NameOnly, Date = lastModified.ToString("dd/MM/yy HH:mm:ss"), FileSize = SizeOut, Identical = false });
+            }
+        }
+
     }
 }
